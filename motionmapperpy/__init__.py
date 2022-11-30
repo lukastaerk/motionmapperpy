0b849c9748976381e360e8cd897559b8c54ac80c
Luka Stärk <luka.staerk@outlook.com>
2022-11-30 12:26:42 +0100

    update

--- motionmapperpy/__init__.py
+++ motionmapperpy/__init__.py
@@ -1,6 +1,6 @@
 from .setrunparameters import setRunParameters
 from .mmutils import findPointDensity, gencmap, createProjectDirectory
-from .motionmapper import run_tSne, run_UMAP, runEmbeddingSubSampling, subsampled_tsne_from_projections, findEmbeddings
+from .motionmapper import run_tSne, run_UMAP, runEmbeddingSubSampling, subsampled_tsne_from_projections, findEmbeddings, set_kmeans_model, findClusters
 from .wavelet import findWavelets
 from .wshed import findWatershedRegions
 from .demoutils import makeregionvideo_flies, getTransitions, makeTransitionMatrix, doTheShannonShuffle, \
